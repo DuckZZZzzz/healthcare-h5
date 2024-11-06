649809300a85757c8ce9c69f184f36b771ab80ed
zzz <792479256@qq.com>
2024-11-06 10:27:38 +0800

    feat: 完成了导航栏和路由跳转

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+
+<van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button plain type="primary">朴素按钮</van-button>
+<van-button plain type="success">朴素按钮</van-button>
+
+</template>
+<script setup>
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <RouterView />
+  <van-tabbar v-model="active">
+    <van-tabbar-item v-for="item in router.options.routes[0].children" :key="item.path" :icon="item.meta.icon"
+      :to="'/' + item.path">
+      {{ item.meta.name }}
+    </van-tabbar-item>
+  </van-tabbar>
+</template>
+<script setup>
+
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+
+const active = ref(0)
+const route = useRoute()
+const router = useRouter()
+
+onMounted(() => {
+  const data = router.options.routes[0]
+  console.log(data.children, route.path)
+  active.value = data.children.findIndex(item => '/' + item.path === route.path)
+})
+
+
+
+</script>
